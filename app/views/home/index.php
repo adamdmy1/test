f4415c998fdf2af22c6fc677f5d442890f1add77
Adam DM <96007048+adamdmy1@users.noreply.github.com>
2022-06-22 00:20:45 +0700

    Add files via upload

---
+++ app/views/home/index.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="<?php echo AST; ?>/css/login.css">
+<div class="card">
+    <div class="card-header">
+        WELCOME
+    </div>
+    <div class="container">
+    <div class="card-body">
+        Selamat Datang di Tugas 2 Pemrograman Berbasis Web Lanjutan tahun 2022
+        <p></p>
+        <div class="card" style="width: 18rem;">
+        <div class="container">
+            <img class="card-img-top" src="<?php echo AST; ?>/images/adam.jpeg" alt="Card image cap">
+        </div>
+            <div class="card-body">
+                <h5 class="card-title">Adam DM 0702193179</h5>
+                <p class="card-text justify">Saya adalah seorang mahasiswa dari Universitas Islam Negeri Sumatera Utara saat Jurusan Sistem Informasi, saat ini saya berada di Semester 6.</p>
+                <a href="#" class="btn btn-primary">More</a>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="<?php echo AST; ?>/images/adamgh.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Akun Github</h5>
+            <p class="card-text">Akun yang digunakan untuk mengumpulkan Tugas 2 Pemrograman Berbasis Web Lanjutan</p>
+            <p class="card-text"><small class="text-muted">Kelas SI-3</small></p>
+        </div>
+    </div>
+</div>
+<script src="<?php echo AST; ?>/js/bootstrap.min.js"></script>
+</div>
